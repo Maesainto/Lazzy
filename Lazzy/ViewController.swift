d26ac8de14ff15414c77fdba4e0afc297bdb647a
maesainto <mae.sainto@gmail.com>
2026-09-25 15:21:03 +0200

    Initial Commit

---
+++ Lazzy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lazzy
+//
+//  Created by etudiant on 25/09/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
